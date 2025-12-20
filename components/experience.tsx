5179e21b2d0913331d997325951a416c059950db
Kunj <184824849+kunjsonker@users.noreply.github.com>
2025-12-21 03:27:42 +0530

    deploy ready bug fix 2

--- components/experience.tsx
+++ components/experience.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import React from "react";
-import SectionHeading from "./section-heading";
-import {
-  VerticalTimeline,
-  VerticalTimelineElement,
-} from "react-vertical-timeline-component";
-import "react-vertical-timeline-component/style.min.css";
-import { experiencesData } from "@/lib/data";
-import { useSectionInView } from "@/lib/hooks";
+// import SectionHeading from "./section-heading";
+// import {
+//   VerticalTimeline,
+//   VerticalTimelineElement,
+// } from "react-vertical-timeline-component";
+// import "react-vertical-timeline-component/style.min.css";
+// import { experiencesData } from "@/lib/data";
+// import { useSectionInView } from "@/lib/hooks";
 
 export default function Experience() {
     // const { ref } = useSectionInView("Experience");
